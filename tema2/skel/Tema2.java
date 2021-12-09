0d185926f2f50bfde1aa24be52559a728e23e3b3
Robert Mrn <rmrn56@gmail.com>
2021-12-09 16:54:19 +0200

    Read input arguments

--- tema2/skel/Tema2.java
+++ tema2/skel/Tema2.java
@@ -2,10 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Tema2 {
+    static Integer threadNumber;
+    static String inputFile;
+    static String outputFile;
     public static void main(String[] args) {
         if (args.length < 3) {
             System.err.println("Usage: Tema2 <workers> <in_file> <out_file>");
             return;
         }
+        // Taking values from args
+        threadNumber = Integer.parseInt(args[0]); // Number of threads
+        inputFile = args[1];// Input file
+        outputFile = args[2];// Output file
+
+        
     }
 }
